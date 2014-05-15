939d7c82d70b0206a55a342423abb821dbbd395b
Manuel Danisch <manuel.danisch@bankmark.de>
2014-05-15 18:14:30 +0200

    simplified showQueryTimes script

--- scripts/showQueryTimes.sh
+++ scripts/showQueryTimes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find $BIG_BENCH_HOME/logs/q??.log -type f -print | xargs grep -A 10 "time ===="
-
+#find $BIG_BENCH_HOME/logs/q??.log -type f -print | xargs grep -A 10 "time ===="
+grep -A 10 "time ====" $BIG_BENCH_HOME/logs/q??.log
