28a46f56aa1fb31ff9bf36f7066a255d03b986d0
Manuel Danisch <manuel.danisch@bankmark.de>
2014-05-15 20:36:40 +0200

    corrected typo

--- hive/run.sh
+++ hive/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 hive -f ${BIG_BENCH_HIVE_SCRIPT_DIR}/dropTables.hql
-hive -f ${BIG_BENCH_HIVE_SCRIPT_DIR}/hiveCreateLoadOCR.hql
+hive -f ${BIG_BENCH_HIVE_SCRIPT_DIR}/hiveCreateLoadORC.hql
 
 hadoop fs -mkdir "$BIG_BENCH_HDFS_ABSOLUTE_TEMP_DIR"
 hadoop fs -mkdir "$BIG_BENCH_HDFS_ABSOLUTE_QUERY_RESULT_DIR"
