1c359b884f3da212838d0359b3bd7112d20031a1
Manuel Danisch <manuel.danisch@bankmark.de>
2014-07-09 12:56:53 +0200

    replaced incorrect usage of SetupQueryParams with easier construct to fix complete script breakage

--- scripts/bigBenchCleanHive.sh
+++ scripts/bigBenchCleanHive.sh
@@ -10,20 +10,9 @@ else
 	source "$ENV_SETTINGS"
 fi
 
-SETUP_PARAMS="`dirname $0`/bigBenchSetupQueryParams.sh"
-if [ ! -f "$SETUP_PARAMS" ]
-then
-	echo "Query parameter setup file $SETUP_PARAMS not found"
-	exit 1
-fi
-
 # write environment information into logfile
 logEnvInformation
 
-run_main_method () {
-	echo "cleaning HIVE TABLES"
-	"$BINARY" $HIVE_PARAMS -i "$COMBINED_PARAMS_FILE" -f "${BIG_BENCH_HIVE_SCRIPT_DIR}/dropTables.sql"
-	rm -rf "$BIG_BENCH_LOADING_STAGE_LOG"
-}
-
-source "$SETUP_PARAMS"
+echo "cleaning HIVE TABLES"
+"$HIVE_BINARY" -f "${BIG_BENCH_HIVE_SCRIPT_DIR}/dropTables.sql"
+rm -rf "$BIG_BENCH_LOADING_STAGE_LOG"
--- scripts/bigBenchPopulateHive.sh
+++ scripts/bigBenchPopulateHive.sh
@@ -10,19 +10,12 @@ else
 	source "$ENV_SETTINGS"
 fi
 
-SETUP_PARAMS="`dirname $0`/bigBenchSetupQueryParams.sh"
-if [ ! -f "$SETUP_PARAMS" ]
-then
-	echo "Query parameter setup file $SETUP_PARAMS not found"
-	exit 1
-fi
-
 # write environment information into logfile
 logEnvInformation
 
 populateHive () {
 	"$BIG_BENCH_BASH_SCRIPT_DIR/bigBenchCleanHive.sh"
-	"$BINARY" $HIVE_PARAMS -i "$COMBINED_PARAMS_FILE" -f "${BIG_BENCH_HIVE_SCRIPT_DIR}/hiveCreateLoadORC.sql"
+	"$HIVE_BINARY" -f "${BIG_BENCH_HIVE_SCRIPT_DIR}/hiveCreateLoadORC.sql"
 
 	hadoop fs -mkdir -p "$BIG_BENCH_HDFS_ABSOLUTE_TEMP_DIR" &
 	hadoop fs -mkdir -p "$BIG_BENCH_HDFS_ABSOLUTE_QUERY_RESULT_DIR" &
@@ -32,18 +25,14 @@ populateHive () {
 	wait
 }
 
-run_main_method () {
-	echo "==============================================="
-	echo "Adding/Updating generated files to HIVE. (drops old tables)"
-	echo "==============================================="
+echo "==============================================="
+echo "Adding/Updating generated files to HIVE. (drops old tables)"
+echo "==============================================="
 
-	time (populateHive ; echo "======= Load data into hive time =========") > >(tee -a "$BIG_BENCH_LOADING_STAGE_LOG") 2>&1 
-	echo "==========================="
+time (populateHive ; echo "======= Load data into hive time =========") > >(tee -a "$BIG_BENCH_LOADING_STAGE_LOG") 2>&1 
+echo "==========================="
 
-	echo "==============================================="
-	echo "HIVE load finished. You may start executing the queries by running script:"
-	echo " bigBenchRunQueries.sh"
-	echo "==============================================="
-}
-
-source "$SETUP_PARAMS"
+echo "==============================================="
+echo "HIVE load finished. You may start executing the queries by running script:"
+echo " bigBenchRunQueries.sh"
+echo "==============================================="
