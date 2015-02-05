a4e6d1e11b072cf2f54065a59e291f4aa57c80fb
Manuel Danisch <manuel.danisch@bankmark.de>
2015-02-05 19:00:18 +0100

    Removed old non-working code from previous kit in killAllHadoopJobs.sh

--- bin/killAllHadoopJobs.sh
+++ bin/killAllHadoopJobs.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-ENV_SETTINGS="`dirname $0`/../setEnvVars"
-if [ ! -f "$ENV_SETTINGS" ]
-then
-        echo "Environment setup file $ENV_SETTINGS not found"
-        exit 1
-else
-        source "$ENV_SETTINGS"
-fi
-
-logEnvInformation
-
 for job in `hadoop job -list 2> /dev/null | grep 'job_' | awk '{print $1}'`; do 
   echo "killing: $job"
   hadoop job -kill $job & 
