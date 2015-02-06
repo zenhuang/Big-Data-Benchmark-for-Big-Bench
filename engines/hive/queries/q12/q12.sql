f37f503ff08a277d4f8c09e037c07258f03fe341
Michael-Frank <michael.frank@bankmark.de>
2015-02-06 17:17:52 +0100

    reworked q12. added more precise query description and adapted SQL accordingly

--- engines/hive/queries/q12/q12.sql
+++ engines/hive/queries/q12/q12.sql
@@ -1,50 +1,8 @@
 --Find all customers who viewed items of a given category on the web
---in a given month and year that was followed by an in-store purchase in the three
+--in a given month and year that was followed by an in-store purchase of an item from the same category in the three
 --consecutive months.
 
 -- Resources
-
---1)
-DROP VIEW IF EXISTS ${hiveconf:TEMP_TABLE1};
-CREATE VIEW IF NOT EXISTS ${hiveconf:TEMP_TABLE1} AS
-SELECT c.wcs_item_sk AS item,
-  c.wcs_user_sk AS uid,
-  c.wcs_click_date_sk AS c_date,
-  c.wcs_click_time_sk AS c_time
-FROM web_clickstreams c
-LEFT SEMI JOIN (
-  SELECT d_date_sk
-  FROM date_dim d
-  WHERE d.d_date >= '${hiveconf:q12_startDate}'
-  AND   d.d_date <= '${hiveconf:q12_endDate1}'
-) dd ON ( c.wcs_click_date_sk=dd.d_date_sk )
-JOIN item i ON c.wcs_item_sk = i.i_item_sk
-WHERE i.i_category IN (${hiveconf:q12_i_category_IN})
-AND c.wcs_user_sk IS NOT NULL
-CLUSTER BY c_date, c_time
-;
-
-
---2)
-DROP VIEW IF EXISTS ${hiveconf:TEMP_TABLE2};
-CREATE VIEW IF NOT EXISTS ${hiveconf:TEMP_TABLE2} AS
-SELECT ss.ss_item_sk AS item,
-  ss.ss_customer_sk AS uid,
-  ss.ss_sold_date_sk AS s_date,
-  ss.ss_sold_time_sk AS s_time
-FROM store_sales ss
-LEFT SEMI JOIN (
-  SELECT d_date_sk
-  FROM date_dim d
-  WHERE d.d_date >= '${hiveconf:q12_startDate}'
-  AND   d.d_date <= '${hiveconf:q12_endDate2}'
-) dd ON ( ss.ss_sold_date_sk=dd.d_date_sk )
-JOIN item i ON ss.ss_item_sk = i.i_item_sk
-WHERE i.i_category IN (${hiveconf:q12_i_category_IN})
-AND ss.ss_customer_sk IS NOT NULL
-CLUSTER BY s_date, s_time
-;
-
 
 --Result  --------------------------------------------------------------------
 --keep result human readable
@@ -55,21 +13,57 @@ DROP TABLE IF EXISTS ${hiveconf:RESULT_TABLE};
 CREATE TABLE ${hiveconf:RESULT_TABLE} (
   c_date BIGINT,
   s_date BIGINT,
-  uid    BIGINT
+  i_id    BIGINT,
+		u_id    BIGINT
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 STORED AS ${env:BIG_BENCH_hive_default_fileformat_result_table}  LOCATION '${hiveconf:RESULT_DIR}';
 
--- the real query part
 INSERT INTO TABLE ${hiveconf:RESULT_TABLE}
-SELECT DISTINCT c_date, s_date, s.uid
-FROM ${hiveconf:TEMP_TABLE1} c
-JOIN ${hiveconf:TEMP_TABLE2} s ON c.uid = s.uid
-WHERE c.c_date < s.s_date
+-- the real query part
+SELECT DISTINCT wcsView.wcs_click_date_sk, 
+                storeView.ss_sold_date_sk,
+                wcsView.wcs_item_sk,
+                wcsView.wcs_user_sk
+FROM( 
+  SELECT wcs.wcs_item_sk,
+         wcs.wcs_user_sk,
+				 	   wcs.wcs_click_date_sk,
+							  i.i_category
+  FROM web_clickstreams wcs
+		-- filter given month and year 
+  LEFT SEMI JOIN (
+    SELECT d_date_sk
+    FROM date_dim d
+    WHERE d.d_date >= '${hiveconf:q12_startDate}'
+    AND   d.d_date <= '${hiveconf:q12_endDate1}'
+  ) dd ON ( wcs.wcs_click_date_sk=dd.d_date_sk )
+		-- filter given category 
+		JOIN item i ON (wcs.wcs_item_sk = i.i_item_sk AND i.i_category IN (${hiveconf:q12_i_category_IN}) )
+  WHERE wcs.wcs_user_sk IS NOT NULL
+) wcsView
+JOIN( 
+  SELECT ss.ss_item_sk,
+         ss.ss_customer_sk,
+         ss.ss_sold_date_sk,
+							  i.i_category
+  FROM store_sales ss
+  LEFT SEMI JOIN (
+    SELECT d_date_sk
+    FROM date_dim d
+				-- filter given month and year + 3 consecutive months
+    WHERE d.d_date >= '${hiveconf:q12_startDate}'
+    AND   d.d_date <= '${hiveconf:q12_endDate2}'
+  ) dd ON ( ss.ss_sold_date_sk=dd.d_date_sk )
+			-- filter given category 
+		JOIN item i ON (ss.ss_item_sk = i.i_item_sk AND i.i_category IN (${hiveconf:q12_i_category_IN})) 
+  WHERE ss.ss_customer_sk IS NOT NULL
+) storeView
+ON (wcs_user_sk = ss_customer_sk)
+	-- filter 3 consecutive months: buy AFTER view on website
+WHERE wcs_click_date_sk < ss_sold_date_sk
+AND wcsView.i_category == storeView.i_category
+CLUSTER BY wcs_click_date_sk,
+           wcs_item_sk,
+										 wcs_user_sk 
 ;
-
---TODO: have to fix partition
-
--- cleanup -------------------------------------------------------------
-DROP VIEW IF EXISTS ${hiveconf:TEMP_TABLE1};
-DROP VIEW IF EXISTS ${hiveconf:TEMP_TABLE2};
