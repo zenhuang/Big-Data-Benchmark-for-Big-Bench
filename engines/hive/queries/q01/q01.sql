3819800419150600884a1da894e62aab4e97cba2
Manuel Danisch <manuel.danisch@bankmark.de>
2015-05-20 15:00:21 -0400

    Added license text to sql files

--- engines/hive/queries/q01/q01.sql
+++ engines/hive/queries/q01/q01.sql
@@ -1,3 +1,11 @@
+--"INTEL CONFIDENTIAL"
+--Copyright 2015  Intel Corporation All Rights Reserved.
+--
+--The source code contained or described herein and all documents related to the source code ("Material") are owned by Intel Corporation or its suppliers or licensors. Title to the Material remains with Intel Corporation or its suppliers and licensors. The Material contains trade secrets and proprietary and confidential information of Intel or its suppliers and licensors. The Material is protected by worldwide copyright and trade secret laws and treaty provisions. No part of the Material may be used, copied, reproduced, modified, published, uploaded, posted, transmitted, distributed, or disclosed in any way without Intel's prior express written permission.
+--
+--No license under any patent, copyright, trade secret or other intellectual property right is granted to or conferred upon you by disclosure or delivery of the Materials, either expressly, by implication, inducement, estoppel or otherwise. Any license under such intellectual property rights must be express and approved by Intel in writing.
+
+
 --Find products that are sold together frequently in given
 --stores. Only products in certain categories sold in specific stores are considered,
 --and "sold together frequently" means at least 50 customers bought these products 
