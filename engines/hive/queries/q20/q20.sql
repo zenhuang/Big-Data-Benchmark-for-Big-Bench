3892e8e03fb62b37fb9ee1f3c9cabe5654e6d496
Michael-Frank <michael.frank@bankmark.de>
2015-07-29 16:57:52 +0000

    double => decimal(15,7) and added sorting to achieve deterministic output. Some minor correctness tweaks.

--- engines/hive/queries/q20/q20.sql
+++ engines/hive/queries/q20/q20.sql
@@ -24,9 +24,9 @@ set hive.exec.compress.output;
 DROP TABLE IF EXISTS ${hiveconf:TEMP_TABLE};
 CREATE TABLE ${hiveconf:TEMP_TABLE} (
   cid            INT,
-  r_order_ratio  DOUBLE,
-  r_item_ratio   DOUBLE,
-  r_amount_ratio DOUBLE,
+  r_order_ratio  decimal(15,7),
+  r_item_ratio   decimal(15,7),
+  r_amount_ratio decimal(15,7),
   r_freq         INT
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n'
@@ -40,35 +40,34 @@ STORED AS TEXTFILE LOCATION '${hiveconf:TEMP_DIR}';
 -- "CASE WHEN <condition> THEN x ELSE 1L" will work because 1L is of type "bigint"
 INSERT INTO TABLE ${hiveconf:TEMP_TABLE}
 SELECT
-  cid,
-  100.0 * COUNT(distinct (CASE WHEN r_date IS NOT NULL THEN oid ELSE 0L END)) / COUNT(distinct oid) AS r_order_ratio,
-  SUM(CASE WHEN r_date IS NOT NULL THEN 1 ELSE 0 END) / COUNT(item) * 100 AS r_item_ratio,
-  CASE WHEN SUM(s_amount)=0.0 THEN 0.0 ELSE (SUM(CASE WHEN r_date IS NOT NULL THEN r_amount ELSE 0.0 END) / SUM(s_amount) * 100) END AS r_amount_ratio,
-  COUNT(distinct (CASE WHEN r_date IS NOT NULL THEN r_date ELSE 0L END)) AS r_freq
-FROM (
-  SELECT --s.ss_sold_date_sk AS s_date, --
-    r.sr_returned_date_sk AS r_date,
-    s.ss_item_sk AS item,
-    s.ss_ticket_number AS oid,
-    s.ss_net_paid AS s_amount,
-    CASE WHEN r.sr_return_amt IS NULL THEN 0.0 ELSE r.sr_return_amt END AS r_amount,
-    (CASE WHEN s.ss_customer_sk  IS NULL THEN r.sr_customer_sk ELSE s.ss_customer_sk END) AS cid
-    --s.ss_customer_sk AS s_cid,
-    --sr_customer_sk AS r_cid
+	  cid,
+	  100.0 * COUNT(distinct (CASE WHEN r_date IS NOT NULL THEN oid ELSE 0L END)) / COUNT(distinct oid) AS r_order_ratio,
+	  SUM(CASE WHEN r_date IS NOT NULL THEN 1 ELSE 0 END) / COUNT(item) * 100 AS r_item_ratio,
+	  CASE WHEN SUM(s_amount)=0.0 THEN 0.0 ELSE (SUM(CASE WHEN r_date IS NOT NULL THEN r_amount ELSE 0.0 END) / SUM(s_amount) * 100) END AS r_amount_ratio,
+	  COUNT(distinct (CASE WHEN r_date IS NOT NULL THEN r_date ELSE 0L END)) AS r_freq
+FROM 
+(
+  SELECT
+		r.sr_returned_date_sk AS r_date,
+		s.ss_item_sk AS item,
+		s.ss_ticket_number AS oid,
+		s.ss_net_paid AS s_amount,
+		CASE WHEN r.sr_return_amt IS NULL THEN 0.0 ELSE r.sr_return_amt END AS r_amount,
+		(CASE WHEN s.ss_customer_sk  IS NULL THEN r.sr_customer_sk ELSE s.ss_customer_sk END) AS cid
+
   FROM store_sales s
-  --LEFT JOIN = LEFT OUTER JOIN
   LEFT OUTER JOIN store_returns r ON (
-    r.sr_item_sk = s.ss_item_sk
-    AND r.sr_ticket_number = s.ss_ticket_number
-    AND s.ss_sold_date_sk IS NOT NULL
-  )
+			r.sr_item_sk = s.ss_item_sk
+			AND r.sr_ticket_number = s.ss_ticket_number
+			AND s.ss_sold_date_sk IS NOT NULL
+		  )
+   WHERE (r.sr_customer_sk IS NOT NULL  OR  s.ss_customer_sk IS NOT NULL)
 ) q20_sales_returns
 
-WHERE cid IS NOT NULL
---Hive does not support GROUP BY 1 (any)
 GROUP BY cid
+HAVING r_freq > 1
+Cluster by cid, r_freq ,r_order_ratio, r_item_ratio, r_amount_ratio
 --(below) error in data: returns NULL
---HAVING COUNT(distinct (CASE WHEN r_date IS NOT NULL THEN r_date ELSE NULL END)) > 1
 ;
 
 ------- Cleanup --------------------------------------
