67dabd448c88fa868f8012b17fcd889e71d9a135
Michael-Frank <michael.frank@bankmark.de>
2015-07-29 20:06:06 +0000

    updated documentation. BigBench will no longer alter the users hive settings by changing parameters with this script. the user has to explicitly enable desired settings.

--- engines/hive/conf/hiveSettings.sql
+++ engines/hive/conf/hiveSettings.sql
@@ -1,6 +1,17 @@
--- !echo ============================;
--- !echo <settings from hiveSettings.sql>;
--- !echo ============================;
+
+--########### READ ME ################
+-- The default way to set hive options is doing it globally for your whole cluster (e.g. cloudera manager, ambari, hive-site.xml, ...)
+-- However, if for some reasons you cant or wont change your cluster global config, you can enable hive specific tuning options in this file.
+-- Below are listed some commonly used settings. The values you see in this file may not apply to your own cluster! we used some of them on our 3 node (16cores 60gb ram) test instances
+--#####################################
+
+
+--###########################
+-- EXECUTION ENGINE
+--###########################
+-- values: mr, tez, spark
+-- set hive.execution.engine=tez;
+
 
 -- ###########################
 -- output and itermediate table settings 
@@ -13,17 +24,18 @@
 -- set hive.exec.compress.output=false;
 -- set mapred.output.compression.codec=org.apache.hadoop.io.compress.DefaultCodec;
 
--- set hive.default.fileformat=TextFile;
+-- set hive.default.fileformat=ORC;
 
 -- ###########################
 -- mappers settings 
 -- ###########################
--- Number of mappers used by HIVE, based on table sizes.
+-- Number of mappers used by HIVE, based on table sizes. If you experience underutilization or to much mappers/reducers, you can play with these settings
 -- The number of physical files a table consists of is irrelevant for hives metric for estimating number of mappers. (Hive uses HiveCombineInputFormat, joining the files)
 -- the following two parameters are most effective in influencing hives estimation of mappers. To low settings may result in to many map tasks, while to high size settings result in to few map tasks and underutilization of the cluster.
 -- both extremes are harmful to the performance. For small data set sizes of 1-100GB a good value  for max.split.size may be 134217728 (128MB). As an estimation, take a medium sized table and divide its size by the number of map tasks you need to utilize your cluster.
--- set mapred.max.split.size=67108864;
--- set mapred.min.split.size=1;
+
+-- set mapreduce.input.fileinputformat.split.minsize=1048576;
+-- set mapreduce.input.fileinputformat.split.maxsize=67108864;
 
 -- ###########################
 -- reducer settings 
@@ -31,28 +43,31 @@
 -- Number of reducers used by HIVE
 -- hives metric for estimating reducers is mostly controlled by the following settings. Node: Some Query functions like count(*) or Distinct will lead to hive always using only 1 reducer
 -- 1GB default
--- set hive.exec.reducers.bytes.per.reducer=256000000;
--- set hive.exec.reducers.max=99999;
+-- set hive.exec.reducers.bytes.per.reducer=33554432;
 
 -- ###########################
 -- optimizations for joins. 
 -- ###########################
 -- things like mapjoins are done in memory and require a lot of it
--- README!!!
+-- README!
 -- Hive 0.12 bug, hive ignores  'hive.mapred.local.mem' resulting in out of memory errors in map joins!
 -- (more exactly: bug in Hadoop 2.2 where hadoop-env.cmd sets the -xmx parameter multiple times, effectively overriding the user set hive.mapred.local.mem setting. see: https://issues.apache.org/jira/browse/HADOOP-10245
 -- There are 3 workarounds: 
 -- 1) assign more memory to the local!! Hadoop JVM client (not! mapred.map.memory)-> map-join child vm will inherit the parents jvm settings
 -- 2) reduce "hive.smalltable.filesize" to ~1MB (depends on your cluster settings for the local JVM)
 -- 3) turn off "hive.auto.convert.join" to prevent hive from converting the join to a mapjoin.
+
 -- MAP join settings:
+-- set hive.auto.convert.join.noconditionaltask.size=100000;
+
 -- set hive.auto.convert.join=true;
 -- set hive.optimize.mapjoin.mapreduce=true;
 -- set hive.mapred.local.mem=1024;
 -- default:25MB, max size of tables considered for local in memory map join. Beware! ORC files have only little file size but huge in memory data size! a 25MB ORC easily consumes 512MB.. related: https://issues.apache.org/jira/browse/HIVE-2601
--- set hive.mapjoin.smalltable.filesize=5000000; 
+-- set hive.mapjoin.smalltable.filesize=10000; 
 -- set hive.mapjoin.localtask.max.memory.usage=0.90;
-
+-- set hive.mapred.local.mem=2147483647;
+-- set mapred.child.java.opts=-Xmx2048m;
 -- set hive.auto.convert.sortmerge.join=true;
 -- set hive.auto.convert.sortmerge.join.noconditionaltask=true;
 -- set hive.auto.convert.join.noconditionaltask.size=100000;
@@ -69,18 +84,26 @@
 -- set hive.exec.parallel=true;
 -- set hive.exec.parallel.thread.number=8;
 
+-- you should really turn these options on for your whole cluster, not just for bigbench
 -- predicate pushdown for ORC-files (eager filtering of columns)
--- set hive.optimize.ppd=false;
--- set hive.optimize.ppd.storage=false;
+-- set hive.optimize.ppd=true;
+-- set hive.optimize.ppd.storage=true;
 -- set hive.ppd.recognizetransivity=false;
 -- set hive.optimize.index.filter=true;
+-- set hive.stats.autogather=true;
+-- set hive.auto.convert.sortmerge.join=true;
+-- set hive.vectorized.execution.enabled=true;
+-- set hive.vectorized.execution.reduce.enabled=true;
+-- set hive.cbo.enable=true;
+-- set hive.compute.query.using.stats=true;
+-- set hive.stats.fetch.column.stats=true;
+-- set hive.stats.fetch.partition.stats=true;
+-- set hive.script.operator.truncate.env=true;
 
 
-
-
--- !echo ============================;
--- !echo Print most important properties;
--- !echo ============================;
+-- ============================;
+-- Print most important properties;
+-- ============================;
 
 set hive.exec.parallel;
 set hive.exec.parallel.thread.number;
@@ -89,8 +112,8 @@ set mapred.map.output.compression.codec;
 set hive.exec.compress.output;
 set mapred.output.compression.codec;
 set hive.default.fileformat;
-set mapred.max.split.size;
-set mapred.min.split.size;
+set mapreduce.input.fileinputformat.split.minsize;
+set mapreduce.input.fileinputformat.split.maxsize;
 set hive.exec.reducers.bytes.per.reducer; 
 set hive.exec.reducers.max;
 set hive.auto.convert.sortmerge.join;
@@ -108,12 +131,10 @@ set hive.mapjoin.localtask.max.memory.usage;
 set hive.optimize.skewjoin;
 set hive.optimize.skewjoin.compiletime;
 set hive.groupby.skewindata;
+set hive.exec.submit.local.task.via.child;
 
--- Database
+-- Database - DO NOT DELETE OR CHANGE
 CREATE DATABASE IF NOT EXISTS ${env:BIG_BENCH_DATABASE};
 use ${env:BIG_BENCH_DATABASE};
 
 
--- !echo ============================;
--- !echo </settings from hiveSettings.sql>;
--- !echo ============================;
