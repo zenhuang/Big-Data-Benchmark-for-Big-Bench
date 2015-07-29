8e240d2d3de2e5397d2e5e267fed876f115f600c
Michael-Frank <michael.frank@bankmark.de>
2015-07-29 16:55:42 +0000

    unified multiple views into one statement

--- engines/hive/queries/q19/run.sh
+++ engines/hive/queries/q19/run.sh
@@ -11,7 +11,7 @@ TEMP_TABLE1="${TEMP_TABLE}_sr_items"
 TEMP_TABLE2="${TEMP_TABLE}_wr_items"
 TEMP_TABLE3="${TEMP_TABLE}_return_items"
 
-BINARY_PARAMS="$BINARY_PARAMS --hiveconf TEMP_TABLE1=$TEMP_TABLE1 --hiveconf TEMP_TABLE2=$TEMP_TABLE2 --hiveconf TEMP_TABLE3=$TEMP_TABLE3"
+BINARY_PARAMS="$BINARY_PARAMS --hiveconf TEMP_TABLE1=$TEMP_TABLE1 "
 
 query_run_main_method () {
 	QUERY_SCRIPT="$QUERY_DIR/$QUERY_NAME.sql"
@@ -28,7 +28,7 @@ query_run_main_method () {
 }
 
 query_run_clean_method () {
-	runCmdWithErrorCheck runEngineCmd -e "DROP VIEW IF EXISTS $TEMP_TABLE1; DROP VIEW IF EXISTS $TEMP_TABLE2; DROP VIEW IF EXISTS $TEMP_TABLE3; DROP TABLE IF EXISTS $RESULT_TABLE;"
+	runCmdWithErrorCheck runEngineCmd -e "DROP TABLE IF EXISTS $RESULT_TABLE;"
 	return $?
 }
 
