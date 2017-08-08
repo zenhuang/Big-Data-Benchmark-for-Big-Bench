66c9d20974859decf842fbe5f168bb966b93c72d
zenhuang <hzpku2011@gmail.com>
2017-08-08 20:36:38 +0800

    fix q14/q17 sql scripts for Spark 2.1.0+

--- engines/hive/queries/q14/q14.sql
+++ engines/hive/queries/q14/q14.sql
@@ -44,7 +44,7 @@ FROM (
   AND wp.wp_char_count >= ${hiveconf:q14_content_len_min}
   AND wp.wp_char_count <= ${hiveconf:q14_content_len_max}
 ) at
-JOIN (
+CROSS JOIN (
   SELECT COUNT(*) pmc
   FROM web_sales ws
   JOIN household_demographics hd ON ws.ws_ship_hdemo_sk = hd.hd_demo_sk
